a3bf298b06d187319386c22ee7b08336481bf5e8
SeredaAI <sereda-2009@mail.ru>
2024-01-11 00:30:07 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'domain.dart';
 import 'firebase_options.dart';
 
@@ -24,9 +27,51 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.userChanges(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return ElevatedButton(
+                onPressed: singIn,
+                child: const Center(
+                  child: Text('Login'),
+                ),
+              );
+            } else {
+              return const MyHomePage(title: 'Flutter Demo Home Page');
+            }
+          }),
     );
   }
+}
+
+void singIn() {
+  if (kIsWeb) {
+  } else {
+    singInWithGoogle();
+  }
+}
+
+Future<UserCredential?> singInWithGoogle() async {
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  print(googleUser?.email);
+  if (googleUser != null) {
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
+
+Future<UserCredential?> singInWithGoogleWeb() async {
+  GoogleAuthProvider googleProvider = GoogleAuthProvider();
+  googleProvider.addScope('https://googelapis.com/auth/contacts.readonly');
+  googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
+  return await FirebaseAuth.instance.signInWithPopup(googleProvider);
 }
 
 class MyHomePage extends StatefulWidget {
@@ -72,67 +117,78 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: StreamBuilder<List<Item>>(
-                stream: _items
-                    .snapshots()
-                    .map((e) => e.docs.map((e) => e.data()).toList()),
-                builder: (context, snapshot) => ListView(
-                      children: snapshot.hasData
-                          ? snapshot.data!
-                              .map((e) => CheckboxListTile(
-                                    title: Text(e.name),
-                                    value: e.purchased,
-                                    onChanged: (value) {
-                                      var newValue = value!;
-                                      FirebaseFirestore.instance
-                                          .collection('items')
-                                          .where('name', isEqualTo: e.name)
-                                          .get()
-                                          .then(
-                                        (value) {
-                                          updateItem(
-                                              docID: value.docs.single.id,
-                                              purchased: newValue);
+          appBar: AppBar(
+              title: SizedBox(
+            width: 100,
+            height: 200,
+            child: FittedBox(
+                fit: BoxFit.fitWidth,
+                child: FutureBuilder<String>(
+                    future: storage.ref('clipart2076047.png').getDownloadURL(),
+                    builder: (context, snapshot) => snapshot.connectionState ==
+                            ConnectionState.done
+                        ? Image.network(snapshot.data!, fit: BoxFit.scaleDown)
+                        : const SizedBox())),
+          )),
+          body: Column(
+            children: [
+              Expanded(
+                child: StreamBuilder<List<Item>>(
+                    stream: _items
+                        .snapshots()
+                        .map((e) => e.docs.map((e) => e.data()).toList()),
+                    builder: (context, snapshot) => ListView(
+                          children: snapshot.hasData
+                              ? snapshot.data!
+                                  .map((e) => CheckboxListTile(
+                                        title: Text(e.name),
+                                        value: e.purchased,
+                                        onChanged: (value) {
+                                          var newValue = value!;
+                                          FirebaseFirestore.instance
+                                              .collection('items')
+                                              .where('name', isEqualTo: e.name)
+                                              .get()
+                                              .then(
+                                            (value) {
+                                              updateItem(
+                                                  docID: value.docs.single.id,
+                                                  purchased: newValue);
+                                            },
+                                          );
                                         },
-                                      );
-                                    },
-                                  ))
-                              .toList()
-                          : [],
-                    )),
-          ),
-          SizedBox(
-            height: 100,
-            child: Row(children: [
-              Flexible(
-                flex: 3,
-                child: TextField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(hintText: 'Item name'),
-                ),
+                                      ))
+                                  .toList()
+                              : [],
+                        )),
               ),
-              Flexible(
-                flex: 1,
-                child: Checkbox(
-                  value: _purchasedController,
-                  onChanged: (value) {
-                    setState(() {
-                      _purchasedController = value!;
-                    });
-                  },
-                ),
-              ),
-              ElevatedButton(onPressed: _submit, child: const Text('Submit'))
-            ]),
-          )
-        ],
-      )),
+              SizedBox(
+                height: 100,
+                child: Row(children: [
+                  Flexible(
+                    flex: 3,
+                    child: TextField(
+                      controller: _nameController,
+                      decoration: const InputDecoration(hintText: 'Item name'),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Checkbox(
+                      value: _purchasedController,
+                      onChanged: (value) {
+                        setState(() {
+                          _purchasedController = value!;
+                        });
+                      },
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: _submit, child: const Text('Submit'))
+                ]),
+              )
+            ],
+          )),
     );
   }
 }
